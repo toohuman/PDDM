cbb050fafdde37ffeb9786e4f5462d8b97294cbd
TooHuman <michael@toohuman.io>
2019-02-13 17:44:37 +0000

    Heatmaps looking good. Cannot match Jonathan's colour map though.

--- plotting/plot_hm_loss.py
+++ plotting/plot_hm_loss.py
@@ -1,12 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use("seaborn-darkgrid")
-import matplotlib.cm as cm
+import seaborn as sns; sns.set()
 
 PERC_LOWER = 10
 PERC_UPPER = 90
-
-cmap = cm.get_cmap("magma")
 
 states_set = [5, 10, 20, 30, 40, 50]
 agents_set = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
@@ -15,16 +12,18 @@ er = 0.01
 
 result_directory = "../../results/test_results/pddm/"
 
-heatmap_results = [[0.0 for x in states_set] for y in agents_set]
-print(heatmap_results)
+heatmap_results = np.array([[0.0 for x in agents_set] for y in states_set])
+labels = [["" for x in agents_set] for y in states_set]
 
-for i, agents in enumerate(agents_set):
-    for j, states in enumerate(states_set):
+for i, states in enumerate(reversed(states_set)):
+    for j, agents in enumerate(agents_set):
         file_name_parts = ["loss", agents, "agents", states, "states", "{:.3f}".format(er), "er"]
         file_ext = ".csv"
         file_name = "_".join(map(lambda x: str(x), file_name_parts)) + file_ext
 
         steady_state_results = []
+
+        labels[i][j] = "{}:{}".format(states, agents)
 
         try:
             with open(result_directory + file_name, "r") as file:
@@ -40,6 +39,21 @@ for i, agents in enumerate(agents_set):
 
         except FileNotFoundError:
             # Add obvious missing entry into final results array here
-            heatmap_results[i][j] = -1.0
+            heatmap_results[i][j] = 1.0
 
 print(heatmap_results)
+print(labels)
+cmap = sns.cubehelix_palette(4, start=0.1, rot=0, dark=0, light=.95, as_cmap=True, reverse=True)
+ax = sns.heatmap(
+    heatmap_results,
+    # center=0,
+    cmap="magma",
+    cbar_kws={"shrink": .75},
+    xticklabels=agents_set,
+    yticklabels=list(reversed(states_set)),
+    vmin=0, vmax=1,
+    linewidths=.5,
+    square=True
+)
+ax.set(xlabel='No. of Agents', ylabel='No. of States')
+plt.show()
