03f94c3eff9c47132cb2d86477f9df42b8e742dd
TooHuman <michael@toohuman.io>
2019-03-29 14:11:23 +0000

    Cleaning up old, commented out code.

--- utilities/operators.py
+++ utilities/operators.py
@@ -1,17 +1,3 @@
-# def combine(prefs1, prefs2):
-#     """
-#     A renormalised sum of the two preference matrices.
-#     """
-
-#     preferences = np.add(prefs1, prefs2)
-#     # Ensure diagonals remain false
-#     np.fill_diagonal(preferences, -1)
-#     # Normalising extreme values: 2, -2.
-#     np.clip(preferences, -1, 1, preferences)
-
-#     return preferences
-
-
 def combine(prefs1, prefs2):
     """
     A renormalised sum of the two preference sets.
@@ -25,38 +11,6 @@ def combine(prefs1, prefs2):
     preferences = set(consistent_prefs)
 
     return preferences
-
-
-# def transitive_closure(matrix):
-#     """
-#     Form the transitive closure of the input matrix by filling in relations
-#     where they are missing.
-#     """
-
-#     # Identify the binary relations of states (p > q)
-#     rows, columns = np.where(matrix == 1)
-#     closure = set([(rows[i], columns[i]) for i in range(len(rows))])
-#     # Copy the initial closure of the preference matrix for comparison at the end
-#     initial_closure = closure.copy()
-
-#     # Identify the transitive relations
-#     # Source: https://stackoverflow.com/questions/8673482/transitive-closure-python-tuples
-#     while True:
-#         new_relations = set((x,w) for x,y in closure for q,w in closure if q == y)
-#         # Form the union of the new relations with the current closure
-#         closure_until_now = closure | new_relations
-
-#         if closure_until_now == closure:
-#             break
-
-#         closure = closure_until_now
-
-#     # Form the transitive closure
-#     for x,y in closure:
-#         matrix[x][y] = 1
-#         matrix[y][x] = -1
-
-#     return True if initial_closure == closure else False
 
 
 def transitive_closure(closure):
--- utilities/results.py
+++ utilities/results.py
@@ -1,45 +1,6 @@
 # This file contains the functions for calculating results from the agents'
 # preferences, as well as writing the results to a file.
 
-# def identify_preference(preferences):
-#     """
-#     """
-
-#     max_pref = 0
-#     preference = []
-#     for i, row in enumerate(preferences):
-#         count = np.count_nonzero(row == 1)
-#         if count > max_pref:
-#             max_pref = count
-#             preference = [i]
-#         elif count == max_pref:
-#             preference.append(i)
-
-#     return sorted(preference)
-
-
-# def loss(preferences, true_preferences, normalised = True):
-#     """
-#     Named after the loss function in machine learning, this function calculates
-#     the sum of the differences of two matrices, scaled by 0.5 so that we count
-#     half-values for having no preference between two elements.
-#     The idea is that we compare the agent's preferences with the true state of
-#     the world and a return value of 0 indicates no differences (100% similarity)
-#     or "zero loss" between the true value and the agents' preferences.
-
-#     The normalisation option uses the worst possible preference (in relation to
-#     the true state of the world) to normalise the values in [0, 1].
-#     """
-
-#     differences = np.subtract(preferences, true_preferences)
-#     differences = np.absolute(differences)
-
-#     if normalised:
-#         normaliser = loss(true_preferences, np.transpose(true_preferences), False)
-#         return np.sum(np.multiply(differences, 0.5)) / normaliser
-
-#     return np.sum(np.multiply(differences, 0.5))
-
 
 def loss(preferences, true_preferences, normalised = True):
     """
